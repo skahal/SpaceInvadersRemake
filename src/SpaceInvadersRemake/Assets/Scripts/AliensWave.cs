c78f94d8b43ed8fe2f796c90c8baa317764e71d8
Diego Giacomelli <giacomelli@gmail.com>
2015-07-19 01:01:35 -0300

    Initial setup using Atari 2600 sprites. Create empty scripts game elements: Alien, AliensWave, Bunker, Cannon (player), Game, Ovni and Projectile. Initial code for AliensWave.

---
+++ src/SpaceInvadersRemake/Assets/Scripts/AliensWave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AliensWave : MonoBehaviour {
+
+	public int Columns = 6;
+	public int Rows = 6;
+	public GameObject[] Aliens;
+	public Vector2 Padding = new Vector2(20, 10);
+
+	public void Setup () {
+
+		if (Aliens.Length < Rows) {
+			Rows = Aliens.Length;
+		}
+
+		var startX = (Columns * Padding.x) / -2;
+
+		for (int x = 0; x < Columns; x++) {
+			var xInc = startX + x * Padding.x;
+
+			for(int y = 0; y < Rows; y++) {
+				var inc = new Vector2(xInc, y * Padding.y);
+				Instantiate(Aliens[y], new Vector2(x, y) + inc, Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ src/SpaceInvadersRemake/Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	private AliensWave m_aliensWave;
+
+	void Awake () {
+		m_aliensWave = GetComponent<AliensWave> ();	
+		Setup ();
+	}
+
+	void Setup()
+	{
+		Debug.Log ("Begin game setup...");
+	
+		m_aliensWave.Setup ();
+		
+		Debug.Log ("Game setup done");
+	}
+}
