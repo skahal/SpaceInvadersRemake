8242d9672a7df8cf04e6cd2eebba9032f739ce7c
Diego Giacomelli <giacomelli@gmail.com>
2015-07-19 23:05:44 -0300

    Projectile movement (WIP).

--- src/SpaceInvadersRemake/Assets/Scripts/Cannon.cs
+++ src/SpaceInvadersRemake/Assets/Scripts/Cannon.cs
@@ -4,18 +4,38 @@ using System.Collections;
 public class Cannon: MonoBehaviour {
 
 	private bool m_canMove = true;
+	private Projectile m_projectile;
 	public float Speed = 0.1f;
+	public GameObject ProjectilePrefab;
+
+	void Awake() {
+		var goProjectile = Instantiate (ProjectilePrefab, transform.position, Quaternion.identity) as GameObject;
+		m_projectile = goProjectile.GetComponent<Projectile> ();
+		m_projectile.gameObject.SetActive (false);
+	}
 
 	void Update () {
+		Move ();
+		Fire ();
+	}
+
+	void Move ()
+	{
 		var direction = (float)(Input.GetAxisRaw ("Horizontal"));
 		var x = transform.position.x;
 
 		if (!m_canMove && direction > 0 == x > 0)
 			return;
-		
+
 		direction *= Speed;
-		
-		transform.position = new  Vector3 (x + direction, transform.position.y);
+		transform.position = new Vector3 (x + direction, transform.position.y);
+	}
+
+	void Fire() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			m_projectile.SetTarget (new Vector2 (transform.position.x, 100));
+			m_projectile.gameObject.SetActive (true);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
--- src/SpaceInvadersRemake/Assets/Scripts/Projectile.cs
+++ src/SpaceInvadersRemake/Assets/Scripts/Projectile.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class Projectile: MonoBehaviour {
+	private Vector2? m_target;
+	public float Speed = .1f;
 
-	// Use this for initialization
-	void Start () {
-	
+	public void SetTarget(Vector2 target) {
+		m_target = target;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (m_target.HasValue) {
+			transform.position = Vector2.Lerp (transform.position, m_target.Value, Time.deltaTime * Speed);
+		}
 	}
 }
