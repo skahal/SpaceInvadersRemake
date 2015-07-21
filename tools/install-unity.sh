6ab50818e885f37cb5873362660d844bd2b624b6
Diego Giacomelli <giacomelli@gmail.com>
2015-07-20 22:43:22 -0300

    Fix Unity package name.

--- tools/install-unity.sh
+++ tools/install-unity.sh
@@ -2,4 +2,4 @@ echo 'Downloading from http://netstorage.unity3d.com/unity/afd2369b692a/MacEdito
 curl -O http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity-5.1.2f1.pkg -target /
