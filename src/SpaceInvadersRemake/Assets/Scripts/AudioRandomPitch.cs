c8f34bfef075528fa84f728f8b632c11f824c87b
Diego Giacomelli <giacomelli@gmail.com>
2015-08-12 21:42:57 -0300

    Added AudioRandomPitch. Start new test sound set set02.

---
+++ src/SpaceInvadersRemake/Assets/Scripts/AudioRandomPitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioRandomPitch : MonoBehaviour {
+
+	public float Low = .95f;
+	public float High = 1.05f;
+
+	void Start() {
+		var audioSource = GetComponent<AudioSource> ();
+		audioSource.pitch = Random.Range (Low, High);
+	}
+}
