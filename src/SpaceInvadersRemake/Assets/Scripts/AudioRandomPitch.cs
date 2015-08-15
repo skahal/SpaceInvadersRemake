823081b9f22d408f528efc587e4e49b99540b5cd
Diego Giacomelli <giacomelli@gmail.com>
2015-08-15 10:50:43 -0300

    Some improvements to random pitch

--- src/SpaceInvadersRemake/Assets/Scripts/AudioRandomPitch.cs
+++ src/SpaceInvadersRemake/Assets/Scripts/AudioRandomPitch.cs
@@ -3,11 +3,23 @@ using System.Collections;
 
 public class AudioRandomPitch : MonoBehaviour {
 
+	private AudioSource m_audioSource;
+	private float m_originalPitch;
+
+	public bool RandomAtStart;
 	public float Low = .95f;
 	public float High = 1.05f;
 
-	void Start() {
-		var audioSource = GetComponent<AudioSource> ();
-		audioSource.pitch = Random.Range (Low, High);
+	void Awake() {
+		m_audioSource = GetComponent<AudioSource> ();
+		m_originalPitch = m_audioSource.pitch;
+
+		if (RandomAtStart) {
+			RandomPitch ();
+		}
+	}
+
+	void RandomPitch() {
+		m_audioSource.pitch = Random.Range (Low, High);
 	}
 }
