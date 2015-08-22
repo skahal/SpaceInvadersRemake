5dfe088e692584986410164249cf71237453ff4e
Diego Giacomelli <giacomelli@gmail.com>
2015-08-22 11:37:42 -0300

    Script to enable/disable juiciness and run it.

---
+++ src/SpaceInvadersRemake/Assets/Scripts/Juiceness.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Juiceness : MonoBehaviour {
+
+	private static bool s_juicenessEnabled;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.J)) {
+			s_juicenessEnabled = !s_juicenessEnabled;
+		}
+	}
+
+	public static void Run(string name, Action action) {
+		if (s_juicenessEnabled) {
+			Debug.LogFormat ("Running juiceness {0}", name);
+			action ();
+		}
+	}
+}
