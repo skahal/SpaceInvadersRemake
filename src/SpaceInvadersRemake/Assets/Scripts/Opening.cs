0e1e5dcc258ad860296f64d9e1075328ff90907e
Diego Giacomelli <giacomelli@gmail.com>
2015-08-27 22:15:00 -0300

    Opening scene first experiment.

---
+++ src/SpaceInvadersRemake/Assets/Scripts/Opening.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Skahal.Threading;
+
+public class Opening : MonoBehaviour {
+	public float DelaytoLoadLevel = 8f;
+
+	void Start() {
+		SHThread.Start (DelaytoLoadLevel, () => {
+			Application.LoadLevel ("Main");
+		});
+	}
+
+	void Update () {
+		if (Input.anyKey) {
+			Application.LoadLevel ("Main");
+		}
+	}
+}
