48e6a669bda91b399d5a1e262ade0c9e9cc0ce5c
Diego Giacomelli <giacomelli@gmail.com>
2015-09-05 09:32:00 -0300

    Text helper to create UI Text in a 3D world point.

---
+++ src/SpaceInvadersRemake/Assets/Scripts/TextHelper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// Text helper.
+/// </summary>
+public static class TextHelper
+{
+	/// <summary>
+	/// Creates the text using the prefab in the world point.
+	/// </summary>
+	/// <param name="text">Text.</param>
+	/// <param name="worldPoint">World point.</param>
+	/// <param name="textPrefab">Text prefab.</param>
+	/// <param name="canvasTransform">Canvas transform.</param>
+	public static Text CreateInWorldPoint(string text, Vector3 worldPoint, GameObject textPrefab, Transform canvasTransform)
+	{
+		Vector2 viewportPoint = Camera.main.WorldToViewportPoint(worldPoint);
+
+		var textComponent = (GameObject.Instantiate (textPrefab) as GameObject).GetComponent<Text>();
+		textComponent.text = text;
+		textComponent.transform.parent = canvasTransform;
+		textComponent.transform.localPosition = Vector3.zero;
+		var rectTransform = textComponent.GetComponent<RectTransform> ();
+
+		rectTransform.anchorMin = viewportPoint;  
+		rectTransform.anchorMax = viewportPoint; 
+
+		return textComponent;
+	}
+}
