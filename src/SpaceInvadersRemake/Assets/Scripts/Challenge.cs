f1c2562b467f0a5f6eab507e86e22c5316dca6da
Diego Giacomelli <giacomelli@gmail.com>
2015-09-12 12:23:04 -0300

    Create Challenge script to allow customize the increment of aliens shoot interval, shoot probability and ovni speed for each new wave.

---
+++ src/SpaceInvadersRemake/Assets/Scripts/Challenge.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vexe.Runtime.Types;
+
+public class Challenge : BetterBehaviour {
+	public ChallengeProperty AlienShootInterval;
+	public ChallengeProperty AlienShootProbability;
+	public ChallengeProperty OvniSpeed;
+
+	void Start() {
+		var game = Game.Instance;
+		var ovni = Ovni.Instance;
+		var level = game.WaveNumber - 1;
+
+		game.AlienShootInterval = AlienShootInterval.GetLevelValue (level);
+		game.AlienShootProbability = AlienShootProbability.GetLevelValue (level);
+		ovni.Speed = OvniSpeed.GetLevelValue (level);
+
+		Debug.LogFormat (
+			"Challenge changed to AlienShootInterval:{0}|AlienShootProbability:{1}|OvniSpeed:{2}",
+			game.AlienShootInterval,
+			game.AlienShootProbability,
+			ovni.Speed);
+	}
+}
---
+++ src/SpaceInvadersRemake/Assets/Scripts/ChallengeProperty.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChallengeProperty {
+	public float StartValue;
+	public float VariationByLevel;
+
+	public float GetLevelValue(int level){
+		return StartValue + (VariationByLevel * level);
+	}
+}
